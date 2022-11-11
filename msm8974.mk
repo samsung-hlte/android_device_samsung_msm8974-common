8be4ea7397d8afc33e39c276b82514af7980b40b
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-11 11:05:05 +0100

    msm8974.mk: drop audio.a2dp.default

--- msm8974.mk
+++ msm8974.mk
@@ -50,7 +50,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
