0357e87c6757c26c1f7b04a46e3d94c73e4f3cec
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-16 10:00:12 +0100

    msm8974-common: audio_service: create own audio service to support a2dp again

--- msm8974.mk
+++ msm8974.mk
@@ -49,7 +49,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service \
+    android.hardware.audio.service.hlte \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
